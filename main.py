cf46533bc12fc9014cc6694440dcc31d12e4ed26
Pablo Mazariegos <mazariegospablo928@gmail.com>
2022-05-03 09:34:57 -0600

    UPDATED main.py
    main.py que interactúa con el animal favorito del usuario.

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 print("Nombre: Pablo Andrés Mazariegos Hernández")
 print("Carné: 1541522")
+print("")
+animal = input("Ingrese su animal favorito: ")
+if animal == "elefante":
+    print("¡Ese también es mi animal favorito!")
+else:
+    print("Es un animal bonito, pero me gustan más los elefantes")
